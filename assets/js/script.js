2a4db8e56e04f90b8d1c92ef053d2d7bbdf2fdde
DavidTJGriffin <david.dtjg2001@gmail.com>
2025-06-09 03:19:17 -0700

    refactor and fix api functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,136 +1,71 @@
 var searchBtn = document.getElementById('search');
 var searchInput = document.getElementById('cityName');
-var searchValue = searchInput.value;
-var apiKey = '3075735f44cc723c6a4ccc3fa4f7a600'
-var lon; //declaring 
+var apiKey = '3075735f44cc723c6a4ccc3fa4f7a600';
+
+var lon;
 var lat;
-var iconEl = document.querySelector("#icon");
-var icon0 = document.querySelector('#icon0');
-var icon1 = document.querySelector('#icon1');
-var icon2 = document.querySelector('#icon2');
-var icon3 = document.querySelector('#icon3');
-var icon4 = document.querySelector('#icon4');
 
+var iconEl = document.querySelector('#icon');
+var icons = [
+  document.querySelector('#icon0'),
+  document.querySelector('#icon1'),
+  document.querySelector('#icon2'),
+  document.querySelector('#icon3'),
+  document.querySelector('#icon4')
+];
 
-
-//Function 
-function getLocation() {
-    searchValue = 'New York'
-
-    console.log("Searching weather for ...", searchValue);
-    var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchValue + "&limit=1&appid=" + apiKey;
-    console.log(apiURL);
-    //Fetch data from the API 
-    fetch(apiURL)
-        .then(function (res) {
-            //console.log(res); 
-            return res.json(); //return response as a  JSON object 
-        }).then(function (data) {
-            console.log("Geocoding Response", data);
-            //Grab lat and long from the api response 
-            lon = data[0].lon; //assigning 
-            lat = data[0].lat;
-            //Display City Name on the HTML page 
-            document.getElementById('city').textContent = data[0].name;
-            //Set in local Storage 
-
-
-            //Another fetch for ONE CALL API
-            getWeather();
-        })
-        .catch(function (error) {
-            console.log("Error", error);
-        })
-};
-getLocation();
-
-function getEnteredLocation() {
-    searchValue = searchInput.value;
-
-    console.log("Searching weather for ...", searchValue);
-    var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchValue + "&limit=1&appid=" + apiKey;
-    console.log(apiURL);
-    //Fetch data from the API 
-    fetch(apiURL)
-        .then(function (res) {
-            //console.log(res); 
-            return res.json(); //return response as a  JSON object 
-        }).then(function (data) {
-            console.log("Geocoding Response", data);
-            //Grab lat and long from the api response 
-            lon = data[0].lon; //assigning 
-            lat = data[0].lat;
-            //Display City Name on the HTML page 
-            document.getElementById('city').textContent = data[0].name;
-            //Set in local Storage 
-
-
-            //Another fetch for ONE CALL API
-            getWeather();
-        })
-        .catch(function (error) {
-            console.log("Error", error);
-        })
-};
-
-
+function getLocation(city) {
+  var apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  fetch(apiURL)
+    .then(res => res.json())
+    .then(data => {
+      if (!data.length) return;
+      lon = data[0].lon;
+      lat = data[0].lat;
+      document.getElementById('city').textContent = data[0].name;
+      getWeather();
+    })
+    .catch(err => console.log('Error', err));
+}
 
 function getWeather() {
-    var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
-    console.log(apiURL);
-    //Fetch data from the API 
-    fetch(apiURL)
-        .then(function (res) {
-            //console.log(res); 
-            return res.json(); //return response as a  JSON object 
-        }).then(function (data) {
-            console.log("One call Response", data);
-            document.getElementById('date').textContent = moment().format('MMMM Do YYYY')
-            console.log(data.current.weather.icon);
-            iconEl.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png');
-            document.getElementById('temp').textContent = 'Temp: ' + data.current.temp + '°F'
-            document.getElementById('wind').textContent = 'Wind: ' + data.current.wind_speed + ' mph'
-            document.getElementById('humidity').textContent = 'Humidity: ' + data.current.humidity + '%'
-            document.getElementById('uvIndex').textContent = 'UV index: ' + data.current.uvi
-            console.log(data.daily[4].dt)
 
-            // Forecast data displaying to page
-            icon0.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.daily[0].weather[0].icon + '@2x.png');
-            document.getElementById('0').textContent = moment.unix(data.daily[0].dt).format("MM/DD/YYYY")
-            document.getElementById('temp0').textContent = 'Temp: ' + data.daily[0].temp.day + '°F';
-            document.getElementById('wind0').textContent = 'Wind: ' + data.daily[0].wind_speed + ' mph'
-            document.getElementById('hum0').textContent = 'Humidity: ' + data.daily[0].humidity + '%'
+ var currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  fetch(currentURL)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('date').textContent = moment.unix(data.dt).format('MMMM Do YYYY');
+      iconEl.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+      document.getElementById('temp').textContent = 'Temp: ' + data.main.temp + '°F';
+      document.getElementById('wind').textContent = 'Wind: ' + data.wind.speed + ' mph';
+      document.getElementById('humidity').textContent = 'Humidity: ' + data.main.humidity + '%';
+    });
 
-            icon1.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.daily[1].weather[0].icon + '@2x.png');
-            document.getElementById('1').textContent = moment.unix(data.daily[1].dt).format("MM/DD/YYYY")
-            document.getElementById('temp1').textContent = 'Temp: ' + data.daily[1].temp.day + '°F';
-            document.getElementById('wind1').textContent = 'Wind: ' + data.daily[1].wind_speed + ' mph'
-            document.getElementById('hum1').textContent = 'Humidity: ' + data.daily[1].humidity + '%'
+  var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  fetch(forecastURL)
+    .then(res => res.json())
+    .then(data => {
+      for (var i = 0; i < 5; i++) {
+        var idx = i * 8;
+        var forecast = data.list[idx];
+        icons[i].setAttribute('src', `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`);
+        document.getElementById(String(i)).textContent = moment.unix(forecast.dt).format('MM/DD/YYYY');
+        document.getElementById('temp' + i).textContent = 'Temp: ' + forecast.main.temp + '°F';
+        document.getElementById('wind' + i).textContent = 'Wind: ' + forecast.wind.speed + ' mph';
+        document.getElementById('hum' + i).textContent = 'Humidity: ' + forecast.main.humidity + '%';
+      }
+    })
+    .catch(err => console.log('Error', err));
+}
 
-            icon2.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.daily[2].weather[0].icon + '@2x.png');
-            document.getElementById('2').textContent = moment.unix(data.daily[2].dt).format("MM/DD/YYYY")
-            document.getElementById('temp2').textContent = 'Temp: ' + data.daily[2].temp.day + '°F';
-            document.getElementById('wind2').textContent = 'Wind: ' + data.daily[2].wind_speed + ' mph'
-            document.getElementById('hum2').textContent = 'Humidity: ' + data.daily[2].humidity + '%'
-
-            icon3.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.daily[3].weather[0].icon + '@2x.png');
-            document.getElementById('3').textContent = moment.unix(data.daily[3].dt).format("MM/DD/YYYY")
-            document.getElementById('temp3').textContent = 'Temp: ' + data.daily[3].temp.day + '°F';
-            document.getElementById('wind3').textContent = 'Wind: ' + data.daily[3].wind_speed + ' mph'
-            document.getElementById('hum3').textContent = 'Humidity: ' + data.daily[3].humidity + '%'
-
-            icon4.setAttribute("src", 'https://openweathermap.org/img/wn/' + data.daily[4].weather[0].icon + '@2x.png');
-            document.getElementById('4').textContent = moment.unix(data.daily[4].dt).format("MM/DD/YYYY")
-            document.getElementById('temp4').textContent = 'Temp: ' + data.daily[4].temp.day + '°F';
-            document.getElementById('wind4').textContent = 'Wind: ' + data.daily[4].wind_speed + ' mph'
-            document.getElementById('hum4').textContent = 'Humidity: ' + data.daily[4].humidity + '%'
-        })
-        .catch(function (error) {
-            console.log("Error", error);
-        })
+function getEnteredLocation() {
+  var city = searchInput.value || 'New York';
+  getLocation(city);
+}
 
 
-};
 //Event listener 
 searchBtn.addEventListener('click', getEnteredLocation);
+
+getLocation('New York');
 
